e87e34fe0250da99504d5c6d23762eb500620c31
Piketes23 <jg46668800@gmail.com>
2024-10-21 18:45:25 -0600

    Primer Comentario

---
+++ api/login/login_view.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def login_view(request):
+    template_view = "login.html"
+    
+    
+    
+    return render(request,template_name=template_view)
+
+
