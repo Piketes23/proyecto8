6a3193495eba1140204165202fad696a410858fb
Piketes23 <jg46668800@gmail.com>
2024-10-28 17:35:13 -0600

    terminado

---
+++ DjangoApi/config.rb
@@ -0,0 +1,10 @@
+# config.rb
+
+# Configuración de Compass
+css_dir = "static/css"         # Carpeta donde se generará el CSS compilado
+sass_dir = "static/sass"       # Carpeta donde están tus archivos SCSS
+images_dir = "static/images"    # Carpeta para imágenes
+javascripts_dir = "static/js"   # Carpeta para archivos JS
+
+# Otras configuraciones opcionales
+output_style = :expanded        # :nested, :expanded, :compact, or :compressed
--- api/views.py
+++ api/views.py
@@ -2,6 +2,26 @@ from django.shortcuts import render
 
 # Create your views here.
 
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import messages
+
+def register(request):
+    if request.method == 'POST':
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+        try:
+            user = User.objects.create_user(username=email, first_name=first_name, last_name=last_name, email=email, password=password)
+            user.save()
+            messages.success(request, 'Registration successful!')
+            return redirect('/login/')  # Asegúrate de que esta ruta exista
+        except Exception as e:
+            messages.error(request, 'Registration failed. Please try again.')
+            return redirect('/register/')  # Asegúrate de que esta ruta exista
+    return render(request, 'register.html')  # Asegúrate de que esta plantilla exista
 
 
     
